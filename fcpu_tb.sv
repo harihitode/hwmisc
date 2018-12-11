325c58e61402fd08cb584de6dc94e98319a53c9b
harihitode <harihitode@gmail.com>
2018-12-11 22:36:05 +0900

    fcpu_tb update

--- fcpu_tb.sv
+++ fcpu_tb.sv
@@ -2,12 +2,15 @@
 
 module fcpu_tb ();
 
-   logic clk = 0;
-   initial forever #5 clk <= ~clk;
+   logic sys_clk = 0;
+   initial forever #10 sys_clk <= ~sys_clk;
+   logic ref_clk = 0;
+   initial forever #5 ref_clk <= ~ref_clk;
    logic nrst = 'b0;
 
    wire  rs_tx_in;
    wire  rs_rx_out;
+   wire  clk;
 
    logic [7:0] send_data  = 'hab;
    logic       send_valid = 'b0;
@@ -19,10 +22,39 @@ module fcpu_tb ();
 
    fcpu fcpu_inst
      (
-      .*
+      .*,
+      .uart_txd_in(rs_tx_in),
+      .uart_rxd_out(rs_rx_out),
+
+      .ddr3_dq(),
+      .ddr3_dqs_n(),
+      .ddr3_dqs_p(),
+      .ddr3_addr(),
+      .ddr3_ba(),
+      .ddr3_ras_n(),
+      .ddr3_cas_n(),
+      .ddr3_we_n(),
+      .ddr3_reset_n(),
+      .ddr3_ck_p(),
+      .ddr3_ck_n(),
+      .ddr3_cke(),
+      .ddr3_cs_n(),
+      .ddr3_dm(),
+      .ddr3_odt(),
+
+      .sys_clk_i(sys_clk),
+      .clk_ref_i(ref_clk),
+      .device_temp_i('b0),
+
+      .init_calib_complete(),
+      .tg_compare_error(),
+      .sys_rst_n(nrst),
+      .ui_clk(clk)
       );
 
-   serial_interface serial_pc_side
+   serial_interface
+     #(.WTIME(16'h40))
+   serial_pc_side
      (
       .clk(clk),
       .uart_txd_in(rs_rx_out),
