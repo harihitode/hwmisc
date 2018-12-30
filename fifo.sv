eaaaf72b44ee74ed4619cf7e1539793e7634d261
harihitode <harihitode@gmail.com>
2018-12-30 20:13:15 +0900

    FIFO supports 1-height fifo

--- fifo.sv
+++ fifo.sv
@@ -22,7 +22,7 @@ module fifo
    localparam logic [1:0]   FIFO_WR = 2'b11;
 
    logic [0:(2**FIFO_DEPTH_W)-1][DATA_W-1:0] mem = '0;
-   logic [FIFO_DEPTH_W-1:0]                  read_pos = 'd0, write_pos = 'd0;
+   logic [((FIFO_DEPTH_W > 0) ? FIFO_DEPTH_W : 1)-1:0] read_pos = 'd0, write_pos = 'd0;
    logic                                     full = 0;
    logic [1:0]                               instruction;
 
@@ -31,11 +31,13 @@ module fifo
    assign b_data = mem[read_pos];
    assign instruction = {a_valid & a_ready, b_valid & b_ready};
 
+   localparam int                            NEXT_POS = (FIFO_DEPTH_W > 0) ? 1 : 0;
+
    always_ff @(posedge clk) begin
       if (nrst) begin
-         if (instruction & FIFO_R) read_pos <= read_pos + 1;
-         if (instruction & FIFO_W) write_pos <= write_pos + 1;
-         if (instruction & FIFO_W) mem[write_pos] <= a_data;
+         if (instruction & FIFO_R) read_pos <= read_pos + NEXT_POS;
+         if (instruction & FIFO_W) write_pos <= write_pos + NEXT_POS;
+         if (instruction & FIFO_W) mem[write_pos] <= a_data + NEXT_POS;
       end else begin
          read_pos <= 'd0;
          write_pos <= 'd0;
@@ -49,8 +51,10 @@ module fifo
            FIFO_R: begin // only read === release full flag
               full <= 0;
            end
-           FIFO_W: begin // when write, there are full danger
-              if ($unsigned(read_pos) == $unsigned((FIFO_DEPTH_W)'(write_pos + 'b1))) full <= 1;
+           FIFO_W: begin
+              if ($unsigned(read_pos) ==
+                  $unsigned(((FIFO_DEPTH_W > 0) ? FIFO_DEPTH_W : 1)'(write_pos + NEXT_POS)))
+                full <= 1;
            end
            default: begin
               full <= full;
